f155a5db9a2952e374c5b976ef31ec40ba4925bb
fptrainnie <rainnierainnier@gmail.com>
2014-10-05 06:17:00 +0700

    ได้หน้าจอสีดำ

---
+++ src/breakoutBounce/BreakoutBounce.java
@@ -0,0 +1,43 @@
+package breakoutBounce;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class BreakoutBounce extends BasicGame{
+
+	public BreakoutBounce(String title) {
+		super(title);
+		
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics arg1) throws SlickException {
+		
+		
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		
+		
+	}
+
+	@Override
+	public void update(GameContainer container, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void main(String[] arsg){
+		try{
+			BreakoutBounce game = new BreakoutBounce("B.O.B");
+			AppGameContainer appgc = new AppGameContainer(game);
+			appgc.setDisplayMode(480, 640, false);
+			appgc.start();
+		} catch(SlickException e){
+			e.printStackTrace();
+		}
+	}
+}
